3bd43467c91b7278c878c9558f61f716676c976e
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-01-10 18:01:54 +0530

    fix: supply provider on server side - supply web3 provider both on client and server side - supply http web3 provider in case metamask is not installed

--- components/web3.js
+++ components/web3.js
@@ -1,3 +1,15 @@
 import Web3 from "web3";
-const web3 = new Web3(window.ethereum);
+
+let web3;
+
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+  //We are in the browser and running metamusk
+  web3 = new Web3(window.ethereum);
+} else {
+  //We are on the server or the user is not running metamask
+  const provider = new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/v3/54e9e6e10adc4444a00d725a13dc345d"
+  );
+  web3 = new Web3(provider);
+}
 export default web3;
