7ebe8a26dfbfadab3f86eeaa8745aae136a1a8cc
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-01-11 19:36:09 +0530

    feat: fetch list of deployed campaigns

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
-export default function Home() {
+import factory from "../components/factory";
+
+export async function getStaticProps(context) {
+  const campaigns = await factory.methods.getDeployedCampaigns().call();
+  return {
+    props: { campaigns }, // will be passed to the page component as props
+  };
+}
+
+const Home = ({ campaigns }) => {
   return (
     <div>
-      <h1>Crowdcoin.</h1>
+      <h1>{campaigns[0]}</h1>
     </div>
   );
-}
+};
+
+export default Home;
