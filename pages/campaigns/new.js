bb9cb6b23159c81c7c0e3e5b1f9d67e23b873c4b
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-01-12 22:43:32 +0530

    feat: add create new campaign form (ongoing) - on submit logs the value to console

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,8 +1,38 @@
 import Layout from "../../components/layout";
+import { useState } from "react";
+import { Form } from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
+import { Input } from "semantic-ui-react";
+
 const CampaignNew = () => {
+  const [minimumContribution, setMinimumContribution] = useState("");
+
+  const handleInput = (event) => {
+    setMinimumContribution(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(minimumContribution);
+  };
+
   return (
     <Layout>
       <h1>New Campaign.</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Field>
+          <label>Minimum Contribution in Wei</label>
+          <Input
+            label="Wei"
+            labelPosition="right"
+            value={minimumContribution}
+            onChange={handleInput}
+          />
+        </Form.Field>
+        <Button type="submit" primary>
+          Submit
+        </Button>
+      </Form>
     </Layout>
   );
 };
