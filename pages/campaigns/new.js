1ac943b0a05d9f3c61932754822ea349167fe922
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-01-12 19:04:26 +0530

    feat: add new campaign component

---
+++ pages/campaigns/new.js
@@ -0,0 +1,9 @@
+import Layout from "../../components/layout";
+const CampaignNew = () => {
+  return (
+    <Layout>
+      <h1>New Campaign.</h1>
+    </Layout>
+  );
+};
+export default CampaignNew;
