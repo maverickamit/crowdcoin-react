99ad05b5821ca2eb344c1ebcdb8aae79f2e87df4
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-01-13 00:27:37 +0530

    feat: implement error handling on form submit - handle various errors on create campaign form submit - display error message to user

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -4,29 +4,46 @@ import { Form } from "semantic-ui-react";
 import { Button } from "semantic-ui-react";
 import { Input } from "semantic-ui-react";
 import factory from "../../components/factory";
+import { Message } from "semantic-ui-react";
 
 const CampaignNew = () => {
   const [minimumContribution, setMinimumContribution] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleInput = (event) => {
     setMinimumContribution(event.target.value);
   };
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
-    const accounts = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-    await factory.methods.createCampaign(minimumContribution).send({
-      from: accounts[0],
-    });
-    setMinimumContribution("");
+    setErrorMessage("");
+    if (minimumContribution) {
+      try {
+        event.preventDefault();
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        await factory.methods.createCampaign(minimumContribution).send({
+          from: accounts[0],
+        });
+        setMinimumContribution("");
+      } catch (err) {
+        if (err.message.includes("invalid BigNumber string")) {
+          setErrorMessage(
+            "Only numbers without any decimal values are allowed."
+          );
+        } else {
+          setErrorMessage(err.message);
+        }
+      }
+    } else {
+      setErrorMessage("Please enter a minimum value.");
+    }
   };
 
   return (
     <Layout>
       <h1>New Campaign.</h1>
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit} error={!!errorMessage}>
         <Form.Field>
           <label>Minimum Contribution in Wei</label>
           <Input
@@ -39,6 +56,11 @@ const CampaignNew = () => {
         <Button type="submit" primary>
           Submit
         </Button>
+        <Message
+          error
+          header="There was some errors with your submission."
+          content={errorMessage}
+        />
       </Form>
     </Layout>
   );
