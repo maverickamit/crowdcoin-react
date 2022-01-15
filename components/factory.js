f8a3a800cc260767582b96222b9693bf69df40b1
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-01-16 00:16:23 +0530

    feat!: update solidity contract

--- components/factory.js
+++ components/factory.js
@@ -2,8 +2,8 @@ import web3 from "./web3";
 import CampaignFactory from "../ethereum/contracts/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
-  JSON.parse(CampaignFactory.interface),
-  "0xC6DBca868ef3b61d6F6fD4F3D322c5f1c0B666f0"
+  CampaignFactory.abi,
+  "0x6BA633c87C419aDF5520Ca245B280a4F6AaFa256"
 );
 
 export default instance;
