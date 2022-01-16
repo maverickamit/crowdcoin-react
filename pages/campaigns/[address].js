b99f3e521acd2646086882b61f7bb86722478c10
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-01-16 12:38:57 +0530

    feat: get summary of a campaign

--- pages/campaigns/[address].js
+++ pages/campaigns/[address].js
@@ -1,5 +1,27 @@
 import { useRouter } from "next/router";
 import Layout from "../../components/layout";
+import Campaign from "../../components/campaign";
+
+export async function getStaticProps({ params }) {
+  const campaign = Campaign(params.address);
+  const summary = await campaign.methods.getSummary().call();
+  return {
+    props: {
+      minimumContribution: summary[0],
+      balance: summary[1],
+      totalRequests: summary[2],
+      contributorsCount: summary[3],
+      manager: summary[4],
+    }, // will be passed to the page component as props
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { address: "" } }], // will be passed to getStaticProps as a parameter
+    fallback: true, // See the "fallback" section below
+  };
+}
 
 const CampaignDetails = () => {
   const router = useRouter();
