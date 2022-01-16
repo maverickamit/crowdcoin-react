b171e75cf738e5a2b37410087d40a2c43990927c
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-01-16 16:20:45 +0530

    feat: display campaign summary - use card components to display summary of a campaign

--- pages/campaigns/[address].js
+++ pages/campaigns/[address].js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import Layout from "../../components/layout";
 import Campaign from "../../components/campaign";
+import { Card } from "semantic-ui-react";
 
 export async function getStaticProps({ params }) {
   const campaign = Campaign(params.address);
@@ -23,13 +24,52 @@ export async function getStaticPaths() {
   };
 }
 
-const CampaignDetails = () => {
+const CampaignDetails = ({
+  minimumContribution,
+  balance,
+  totalRequests,
+  contributorsCount,
+  manager,
+}) => {
   const router = useRouter();
   const { address } = router.query;
 
+  const items = [
+    {
+      header: manager,
+      meta: "Address of Manager",
+      description: "The manager created this campaign and can create requests.",
+      style: { overflowWrap: "break-word" },
+    },
+    {
+      header: minimumContribution,
+      meta: "Minimum contribution (wei)",
+      description:
+        "You must contribute at least this much wei to be a contributor.",
+    },
+    {
+      header: totalRequests,
+      meta: "Number of requests",
+      description:
+        "A request tries to withdraw money from the campaign. Requests must be approved by the contributors.",
+    },
+    {
+      header: contributorsCount,
+      meta: "Number of contributors",
+      description:
+        "Number of people who contributed at least the minimum amount to the campaign.",
+    },
+    {
+      header: balance,
+      meta: "Campaign balance (wei)",
+      description: "The balance is how much money the campaign has right now.",
+    },
+  ];
+
   return (
     <Layout>
-      <h1>Campaign Details: {address}</h1>
+      <h3>Campaign Details: {address}</h3>
+      <Card.Group items={items}></Card.Group>
     </Layout>
   );
 };
