be62a7682c0b64a7f7283dd6dabcaee50cfe30e1
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-01-18 22:28:10 +0530

    feat: add button that links to create request page

--- pages/campaigns/[address]/requests.js
+++ pages/campaigns/[address]/requests.js
@@ -1,9 +1,23 @@
 import Layout from "../../../components/layout";
+import Link from "next/link";
+import { Button } from "semantic-ui-react";
 
-const RequestsDetails = () => {
+export async function getServerSideProps({ params }) {
+  const address = params.address;
+  return {
+    props: {
+      address,
+    },
+  };
+}
+
+const RequestsDetails = ({ address }) => {
   return (
     <Layout>
       <h3>Requests List</h3>
+      <Link href={`/campaigns/${address}/requests/new`}>
+        <Button primary>Create Request</Button>
+      </Link>
     </Layout>
   );
 };
