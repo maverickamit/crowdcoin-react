fc793e5fd26aad9a94431310687fea378f6b9f8c
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-01-18 10:57:33 +0530

    feat: add requests details component

---
+++ pages/campaigns/[address]/requests.js
@@ -0,0 +1,11 @@
+import Layout from "../../../components/layout";
+
+const RequestsDetails = () => {
+  return (
+    <Layout>
+      <h3>Requests List</h3>
+    </Layout>
+  );
+};
+
+export default RequestsDetails;
